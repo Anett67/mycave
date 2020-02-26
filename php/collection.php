0d96efa10713a5c8b31ca5bed5eef385a5c6af01
Anett David <davidanett@gmail.com>
2020-02-26 11:58:08 +0100

    Seperate selection form in a new file

--- php/collection.php
+++ php/collection.php
@@ -7,46 +7,17 @@
     <h1>Collection</h2>
     <div class="flex-class">
         <div class="selection">
-            <form id="selection_form" action="../request/select_post.php" method="post">
-
-                <label for="country">Select country</label>
-                <select name="country_choose" id="country_choose">
-                    <option value="0">Choose a country</option>
-                </select>
-                
-                <label for="year_choose">Select year</label>
-                <select name="year_choose" id="year_choose">
-                    <option value="0">Choose the year</option>
-                </select>
-
-                <label for="grapes">Select grapes</label>
-                <select name="grapes_choose" id="grapes_choose">
-                    <option value="0">Select grapes</option>
-                </select>
-
-                <button type="submit">APPLY FILTERS</button>
-
-            </form>
-            <?php 
-                if(isset($_SESSION['id'])):
-             ?>
-            <div class="buttons">
-                <button id="add_new_button">ADD NEW WINE</button>
-                <button type="submit">EDIT</button>
-                
-                <form action="">
-                     <button type="submit">DELETE</button>
-                </form>
-            </div>
-            <?php endif; ?>
+            
+            <!-- LA formulaire qui permet de trier les produits -->
+            <?php require 'selection_form.php'; ?>
             
             <?php if(isset($_GET['response'])){
 
                     echo '<div id="response">' . $_GET['response'] . '</div>';
                     } 
-                ?>
+            ?>
 
-            <!-- Formulaire pour modifier et rajouter des produits -->
+            <!-- Formulaire pour modifier et rajouter des produits avec description, année et photo -->
             <form action="../request/form_post.php" method="post" class="add_new" id="choose_bottle" enctype="multipart/form-data">
 
                 <h2>Add New Product</h2>
---
+++ php/selection_form.php
@@ -0,0 +1,32 @@
+            <form id="selection_form" action="../request/select_post.php" method="post">
+
+                <label for="country">Select country</label>
+                <select name="country_choose" id="country_choose">
+                    <option value="0">Choose a country</option>
+                </select>
+                
+                <label for="year_choose">Select year</label>
+                <select name="year_choose" id="year_choose">
+                    <option value="0">Choose the year</option>
+                </select>
+
+                <label for="grapes">Select grapes</label>
+                <select name="grapes_choose" id="grapes_choose">
+                    <option value="0">Select grapes</option>
+                </select>
+
+                <button type="submit">APPLY FILTERS</button>
+
+            </form>
+            <?php 
+                if(isset($_SESSION['id'])):
+             ?>
+            <div class="buttons">
+                <button id="add_new_button">ADD NEW WINE</button>
+                <button type="submit">EDIT</button>
+                
+                <form action="">
+                     <button type="submit">DELETE</button>
+                </form>
+            </div>
+            <?php endif; ?>
