0b8e791ac3bc512faddb8c734e9609ebda216021
Anett David <davidanett@gmail.com>
2020-02-26 10:01:35 +0100

    Display products on collection page

--- php/collection.php
+++ php/collection.php
@@ -1,113 +1,120 @@
 <?php require '../request/param.php' ?>
 <?php require 'header.php'; ?>
 
+
+
 <div class="products">
     <h1>Collection</h2>
-    <div class="selection">
-        <form id="selection_form" action="../request/select-post.php" method="post">
+    <div class="flex-class">
+        <div class="selection">
+            <form id="selection_form" action="../request/select-post.php" method="post">
 
-            <label for="country">Select country</label>
-            <select name="country_choose" id="country_choose">
-                <option value="0">Choose a country</option>
-            </select>
-            
-            <label for="year_choose">Select year</label>
-            <select name="year_choose" id="year_choose">
-                <option value="0">Choose the year</option>
-            </select>
+                <label for="country">Select country</label>
+                <select name="country_choose" id="country_choose">
+                    <option value="0">Choose a country</option>
+                </select>
+                
+                <label for="year_choose">Select year</label>
+                <select name="year_choose" id="year_choose">
+                    <option value="0">Choose the year</option>
+                </select>
 
-            <label for="grapes">Select grapes</label>
-            <select name="grapes_choose" id="grapes_choose">
-                <option value="0">Select grapes</option>
-            </select>
+                <label for="grapes">Select grapes</label>
+                <select name="grapes_choose" id="grapes_choose">
+                    <option value="0">Select grapes</option>
+                </select>
 
-            <button type="submit">APPLY FILTERS</button>
+                <button type="submit">APPLY FILTERS</button>
 
-        </form>
-        <?php 
-            if(isset($_SESSION['id'])):
-         ?>
-        <div class="buttons">
-            <button id="add_new_button">ADD NEW WINE</button>
-            <button type="submit">EDIT</button>
-            
-            <form action="">
-                 <button type="submit">DELETE</button>
             </form>
-        </div>
-        <?php endif; ?>
-        
-        <?php if(isset($_GET['response'])){
-
-                echo '<div id="response">' . $_GET['response'] . '</div>';
-                } 
-            ?>
-
-        <!-- Formulaire pour modifier et rajouter des produits -->
-        <form action="../request/form_post.php" method="post" class="add_new" id="choose_bottle" enctype="multipart/form-data">
-
-            <h2>Add New Product</h2>
-            
-            <label for="select_bottle">Choose a bottle or add a new one</label>
-            <select name="select_bottle" id="select_bottle">
-
-                <option value="0">Choose a bottle</option>
-                <?php require '../request/bottle_select.php'; ?>
-
-            </select>
-            
-            <div class="bottle_buttons">
-                <button class="add_bottle">Add new bottle</button>
+            <?php 
+                if(isset($_SESSION['id'])):
+             ?>
+            <div class="buttons">
+                <button id="add_new_button">ADD NEW WINE</button>
+                <button type="submit">EDIT</button>
+                
+                <form action="">
+                     <button type="submit">DELETE</button>
+                </form>
             </div>
-
-            <label for="year_new">Year</label>
-            <input type="number" name="year_new" id="year_new">                
-
-            <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10"></textarea>
-
-            <label for="file-upload" class="custom-file-upload">
-                <p>Add photo</p>
-            </label>
-            <input id="file-upload" type="file" name="file_upload" />
+            <?php endif; ?>
             
-            <button type="submit">Save</button>
-            <button id="cancel">Cancel</button>
-        </form>
+            <?php if(isset($_GET['response'])){
 
-            
-        <!-- FORMULAIRE POUR ENREGISTRER UNE NOUVELLE BOUTEILLE -->
+                    echo '<div id="response">' . $_GET['response'] . '</div>';
+                    } 
+                ?>
 
-        <form action="../request/new_bottle_post.php" method="post" class="add_new" id="new_bottle">
-            
-            <h2>Add New Bottle</h2>
+            <!-- Formulaire pour modifier et rajouter des produits -->
+            <form action="../request/form_post.php" method="post" class="add_new" id="choose_bottle" enctype="multipart/form-data">
 
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name">
+                <h2>Add New Product</h2>
+                
+                <label for="select_bottle">Choose a bottle or add a new one</label>
+                <select name="select_bottle" id="select_bottle">
 
-            <label for="new_country">Country</label>
-            <input type="text" id="new_country" name="new_country">
+                    <option value="0">Choose a bottle</option>
+                    <?php require '../request/bottle_select.php'; ?>
 
-            <label for="region">Region</label>
-            <input type="text" id="region" name="region">
+                </select>
+                
+                <div class="bottle_buttons">
+                    <button class="add_bottle">Add new bottle</button>
+                </div>
 
-            <label for="new_grapes">Grapes</label>
-            <input type="text" id="new_grapes" name="new_grapes">
-            
-            <div class="bottle_buttons">
-                <button class="cancel_button">Cancel</button>
+                <label for="year_new">Year</label>
+                <input type="number" name="year_new" id="year_new">                
+
+                <label for="description">Description</label>
+                <textarea name="description" id="description" cols="30" rows="10"></textarea>
+
+                <label for="file-upload" class="custom-file-upload">
+                    <p>Add photo</p>
+                </label>
+                <input id="file-upload" type="file" name="file_upload" />
+                
                 <button type="submit">Save</button>
-            </div>
-        </form>                
+                <button id="cancel">Cancel</button>
+            </form>
+
+                
+            <!-- FORMULAIRE POUR ENREGISTRER UNE NOUVELLE BOUTEILLE -->
+
+            <form action="../request/new_bottle_post.php" method="post" class="add_new" id="new_bottle">
+                
+                <h2>Add New Bottle</h2>
+
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name">
+
+                <label for="new_country">Country</label>
+                <input type="text" id="new_country" name="new_country">
+
+                <label for="region">Region</label>
+                <input type="text" id="region" name="region">
+
+                <label for="new_grapes">Grapes</label>
+                <input type="text" id="new_grapes" name="new_grapes">
+                
+                <div class="bottle_buttons">
+                    <button class="cancel_button">Cancel</button>
+                    <button type="submit">Save</button>
+                </div>
+            </form>                
 
 
+        </div>
+
+        <div class="product_list">
+        
+            <?php require '../request/products_read.php'; ?>
+        </div>   
+
+        
     </div>
     
-    <div class="product_list">
-        
-        
-
-    </div>   
+    
 
 
 </div>
---
+++ request/products_read.php
@@ -0,0 +1,34 @@
+<?php 
+
+require 'connect.php'; 
+
+$req = $bdd->prepare('  
+    SELECT  b.bottle_name, b.grapes, b.country, b.region, bc.year, bc.description, bc.file_url
+    FROM bottles AS b
+    INNER JOIN bottle_collection AS bc   
+    ON  bc.bottle_id = b.id
+');
+
+$req->execute();
+
+while($data = $req->fetch()){ ?>
+    
+<div class="product_container">
+    <img class="bottle_image" src="<?php echo 'http://localhost/mycave/upload/' . $data['file_url']; ?> " alt="photo of wine bottle">
+    <h4><?php echo $data['bottle_name']; ?></h4>
+    <p><?php echo $data['grapes']; ?></p>
+    <p><?php echo $data['country']; ?></p>
+    <p><?php echo $data['region']; ?></p>
+    <p><?php echo $data['year']; ?></p>
+    <div class="description">
+        <p><?php echo $data['description']; ?></p>
+    </div>
+    <a href="">Read More</a>
+</div>
+
+
+<?php
+
+}
+
+?>
